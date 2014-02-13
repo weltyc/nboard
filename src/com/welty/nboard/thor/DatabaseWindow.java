24145c7f9e1686085e73650025c76566cb10a72d
Chris Welty <chris.welty.1@gmail.com>
2014-02-13 13:22:18 -0600

    Add Database filter boxes (not hooked up though)

--- src/com/welty/nboard/thor/DatabaseWindow.java
+++ src/com/welty/nboard/thor/DatabaseWindow.java
@@ -1,5 +1,7 @@
 package com.welty.nboard.thor;
 
+import com.orbanova.common.jsb.Grid;
+import com.orbanova.common.jsb.JSwingBuilder;
 import com.welty.nboard.nboard.ReversiData;
 import com.welty.nboard.nboard.ReversiWindow;
 import org.jetbrains.annotations.NotNull;
@@ -7,32 +9,50 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
-import javax.swing.table.TableModel;
+import java.awt.*;
 
 /**
  * Displays the games in tabular format
  */
 class DatabaseWindow extends JFrame implements TableModelListener {
-    private final DatabaseTable databaseTable;
-    private @NotNull final DatabaseTableModel databaseTableModel;
+    private final DatabaseTable table;
+    private @NotNull final DatabaseTableModel tableModel;
 
-    public DatabaseWindow(ReversiWindow reversiWindow, ReversiData reversiData, @NotNull DatabaseTableModel dd) {
+    public DatabaseWindow(ReversiWindow reversiWindow, ReversiData reversiData, @NotNull DatabaseTableModel tableModel) {
         super("Database");
-        databaseTableModel = dd;
         setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-        databaseTable = new DatabaseTable(reversiWindow, reversiData, dd);
-        add(databaseTable);
+
+        this.tableModel = tableModel;
+        table = new DatabaseTable(reversiWindow, reversiData, tableModel);
+
+        add(JSwingBuilder.vBox(
+                createFilterBoxes(),
+                table
+        )
+        );
         pack();
-        final TableModel tableModel = databaseTable.getTableModel();
+
         tableModel.addTableModelListener(this);
+    }
+
+    private Grid<Component> createFilterBoxes() {
+        final Grid<Component> filterBoxes = JSwingBuilder.hBox();
+        for (int i = 0; i < tableModel.getColumnCount(); i++) {
+            final JTextField input = new JTextField();
+            final int width = tableModel.getColumnWidth(i);
+            input.setPreferredSize(new Dimension(width, 24));
+            input.setFont(table.getFont());
+            filterBoxes.add(input);
+        }
+        return filterBoxes;
     }
 
     /**
      * Invalidate the data area. If all data files have been loaded, show the window
      */
     void ShowIfReady() {
-        databaseTable.repaint();
-        final boolean isReady = databaseTableModel.IsReady();
+        table.repaint();
+        final boolean isReady = tableModel.IsReady();
         if (isReady && !isVisible()) {
             pack();
             setVisible(true);
@@ -40,7 +60,7 @@ class DatabaseWindow extends JFrame implements TableModelListener {
     }
 
     public void tableChanged(TableModelEvent e) {
-        setTitle("Database (" + databaseTableModel.getStatusString() + ")");
+        setTitle("Database (" + tableModel.getStatusString() + ")");
         ShowIfReady();
     }
 }
