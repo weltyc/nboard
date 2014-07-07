c32b9e1dbcd4011f0169174df59e8f25b4bec3aa
Chris Welty <chris.welty.1@gmail.com>
2014-07-07 10:33:42 -0500

    Add GPL v3 license

--- test/com/welty/nboard/nboard/startpos/StartPositionChooserTest.java
+++ test/com/welty/nboard/nboard/startpos/StartPositionChooserTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (c) 2014 Chris Welty.
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, version 3,
+ * as published by the Free Software Foundation.
+ *
+ * This file is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * For the license, see <http://www.gnu.org/licenses/gpl.html>.
+ */
+
 package com.welty.nboard.nboard.startpos;
 
 import com.welty.novello.core.Board;
